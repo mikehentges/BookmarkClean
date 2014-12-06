db8ac154f8267160afc0af581294c6f36d7b7a7b
Michael Hentges <mhentges@comcast.net>
2014-12-05 18:12:24 -0600

    BookmarkClean
    script to clean up a bookmark.html file

--- BookmarkClean.py
+++ BookmarkClean.py
@@ -5,12 +5,14 @@ import lxml.html
 from lxml.html import etree
 # from lxml.html.soupparser import etree
 
-
 print("hello world")
 
 parser = lxml.html.HTMLParser()
 tree = etree.parse('e:\\users\\mike\\documents\\bookmarks_12_5_14_small.html', parser)
 root = tree.getroot()
+
+
+
 
 for element in root.iter('a'):
     print("%s - %s - %s" % (element.tag, element.text, element.attrib))
