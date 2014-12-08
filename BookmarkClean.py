52bed14a289c905c484cf0a7e6a9605c125447b5
Michael Hentges <mhentges@comcast.net>
2014-12-07 20:46:04 -0600

    What worked on the mac.

--- BookmarkClean.py
+++ BookmarkClean.py
@@ -19,6 +19,25 @@ for element in root.iter('a'):
 
 print("hello world")
 
+clean_links = {}
+pre_text = "<DT><A "
+post_text = "</A>"
+
+for element in root.iter("a"):
+    # print("%s - %s - %s" % (element.tag, element.attrib, element.text))
+    clean_links[element.text] = element.attrib
+    # if (len(clean_links) > 10) :
+    # break
+
+for link in sorted(clean_links.keys()):
+    line = pre_text
+    for attrib_key in clean_links[link].keys():
+        line += " "
+        line += str(attrib_key).upper() + "=" + '"'
+        line += clean_links[link][attrib_key]
+        line += '"'
+    line += ">" + link + post_text
+    print(line)
 
 
 
